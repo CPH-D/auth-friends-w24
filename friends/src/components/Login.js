2ee9fbd4298307811abd00cf65d2f70386915914
Christopher Hernandez <cerberean661@gmail.com>
2019-11-12 22:32:27 -0800

    received post response with login test

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+// username: Lambda School
+// password: i<3Lambd4
 
 const Login = (props) => {
     const [creds, setCreds] = useState({ username: "", password: ""});
     const handleChange = event => {
         setCreds({...creds, [event.target.name]: event.target.value})
-    }
+    };
+
+    // when we submit (username+password), we're going to send a post request and get a response with console.log
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/api/login', creds)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => console.log(err.respons));
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" 
                    name="username"
                    placeholder="Enter your username..." 
