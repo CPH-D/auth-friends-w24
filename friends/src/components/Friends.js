83b1df6327082472c08910fedd6a1ea107e0a8bf
Christopher Hernandez <cerberean661@gmail.com>
2019-11-13 11:22:22 -0800

    token authorization passed for axios get

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Friends = (props) => {
+    const [friendsList, setFriendsList] = useState([]);
+    
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/friends', {
+            headers: {
+                Authorization: localStorage.getItem('token')
+            }
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => console.log(err.response));
+    }, []);
+
     return (
         <div>
             <h2>Friends</h2>
         </div>
     )
-}
+};
 
 export default Friends;
