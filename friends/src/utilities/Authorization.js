fc7793a2c3965dce3fb0202dfd8c05f3726e0e93
Christopher Hernandez <cerberean661@gmail.com>
2019-11-13 12:05:11 -0800

    PrivateRoute created

--- friends/src/utilities/Authorization.js
+++ friends/src/utilities/Authorization.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const axiosWithAuth = () => {
+    // .create will return something that will work exactly the same as plain axios
     return axios.create({
         headers: {
             Authorization: localStorage.getItem('token')
