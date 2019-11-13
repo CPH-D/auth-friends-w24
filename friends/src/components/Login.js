3a7ee455a91b9c4d44f4fc897cbb17ebc013b7fb
Christopher Hernandez <cerberean661@gmail.com>
2019-11-13 10:55:00 -0800

    log into friends page successful

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 // password: i<3Lambd4
 
 const Login = (props) => {
+    console.log(props);
     const [creds, setCreds] = useState({ username: "", password: ""});
     const handleChange = event => {
         setCreds({...creds, [event.target.name]: event.target.value})
@@ -14,9 +15,14 @@ const Login = (props) => {
     const handleSubmit = event => {
         event.preventDefault();
         axios.post('http://localhost:5000/api/login', creds)
+        // if successful, then...
         .then(res => {
             console.log(res);
+            // saving token to local storage. with token, we're able to persist user login session between refreshes
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/friends');
         })
+        // if it fails, then do this...
         .catch(err => console.log(err.respons));
     };
 
